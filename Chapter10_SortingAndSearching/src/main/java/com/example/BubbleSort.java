79caffe89afb43cd1ef4d750b0a6c3392799cc22
Ajmal Cholassery <ajmalch@gmail.com>
2024-01-07 21:19:51 -0500

    Bubble sort

--- Chapter10_SortingAndSearching/src/main/java/com/example/BubbleSort.java
+++ Chapter10_SortingAndSearching/src/main/java/com/example/BubbleSort.java
@@ -5,10 +5,13 @@ package com.example;
  */
 public class BubbleSort {
 
-    public int[] bubbleSort(int[] array){
-        for(int i=0 ;i<array.length-1; i++){
+    public static int[] bubbleSort(int[] array) {
+        boolean sorted = false;
+        for(int i=0 ;i<array.length-1 && !sorted; i++){
+            sorted = true; //assuming sorted = true
             for(int j = 0 ; j < array.length-1-i; j++){
                 if(array[j] > array[j+1]){
+                    sorted = false;
                     int temp = array[j];
                     array[j] =  array[j+1];
                     array[j+1] = temp;
