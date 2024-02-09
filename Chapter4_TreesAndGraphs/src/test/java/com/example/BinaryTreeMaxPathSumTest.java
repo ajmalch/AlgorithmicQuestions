400e8b34db2afaffe69a6e481018e29c9a253684
Ajmal Cholassery <ajmalch@gmail.com>
2024-02-09 09:09:44 -0500

    added test for max path sum

--- Chapter4_TreesAndGraphs/src/test/java/com/example/BinaryTreeMaxPathSumTest.java
+++ Chapter4_TreesAndGraphs/src/test/java/com/example/BinaryTreeMaxPathSumTest.java
@@ -1,17 +1,17 @@
 package com.example;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class BinaryTreePathsTest {
+class BinaryTreeMaxPathSumTest {
 
-    private final BinaryTreePaths binaryTreePaths = new BinaryTreePaths();
+    private final BinaryTreeMaxPathSum binaryTreeMaxPathSum = new BinaryTreeMaxPathSum();
 
     @Test
-    void binaryTreePathsTest() {
-             /*
+    void binaryTreeMaxPathSumTeast() {
+
+           /*
                     1
                    / \
                   2   3
@@ -34,11 +34,6 @@ class BinaryTreePathsTest {
         root.right.right.left.right = new TreeNode(9);
         root.right.right.right = new TreeNode(10);
 
-
-        final List<String> paths = binaryTreePaths.binaryTreePaths(root);
-        Assertions.assertEquals(4, paths.size());
-        Assertions.assertTrue(paths
-                .containsAll(List.of("1->2", "1->3->4->6->7", "1->3->5->8->9", "1->3->5->10")));
-
+        assertEquals(42, binaryTreeMaxPathSum.maxPathSum(root));
     }
 }
